d133e6d1f01df365199b0ba15782a8000e10e968
knyl2013 <knyl2013@gmail.com>
2020-02-20 18:01:31 +0800

    Initial Commit

---
+++ NewsRate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsRate
+//
+//  Created by waiyip on 20/2/2020.
+//  Copyright © 2020 waiyip. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
